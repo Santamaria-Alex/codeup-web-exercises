53a1f9a75f3beff5540c4fef5c2a6572724e3a20
Alex Santamaria <santamariaalex146@gmail.com>
2021-04-27 23:56:41 -0500

    assessment practice

---
+++ js/for-loops-practice.js
@@ -0,0 +1,11 @@
+function showMultiplicationTable (input) {
+    for (i = 0; i <= 10; i++) {
+        console.log(input + " x " + i + " = " + input);
+    }
+}
+
+showMultiplicationTable(14);
+
+function filterNumbers([]) {
+
+}
