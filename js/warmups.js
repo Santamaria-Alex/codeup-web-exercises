330512c23675decfb3f59cf97bf6d8c2f9e7bd9a
Alex Santamaria <santamariaalex146@gmail.com>
2021-05-24 09:34:17 -0500

    FizzBuzz warmup

---
+++ js/warmups.js
@@ -0,0 +1,49 @@
+
+
+/**
+ *
+ * Often in programming interviews, you may be asked to solve a problem called “FizzBuzz.” FizzBuzz is a test of your ability to
+ * use conditional logic with a loop that counts up by one with every iteration. The challenge asks that you log the word “Fizz” if
+ * a number is evenly divisible by 3, “Buzz” if a number is evenly divisible by 5, “FizzBuzz” if it is divisible by both 3 and 5,
+ * and finally the number if it is not divisible by 3 or 5.
+ *
+ * This morning, I’d like you to write FizzBuzz in JavaScript using a for loop that goes until the number 30.
+ *
+ */
+
+
+////// My answer -- was correct /////
+function fizzBuzz (){
+    for (var i = 1; i <= 30; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+        } else if (i % 3 === 0){
+            console.log("Fizz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// fizzBuzz();
+
+
+///// Professor answer /////
+(() => {
+    const fizzBuzz = numTil => {
+        for (let x =1; x <= numTil; x++){
+            if (x % 3 === 0 && x % 5 === 0){
+                console.log("FizzBuzz");
+            } else if (x % 3 === 0){
+                console.log("Fizz");
+            } else if (x % 5 === 0){
+                console.log("Buzz")
+            } else {
+                console.log(x);
+            }
+        }
+    }
+    fizzBuzz(30);
+})();
